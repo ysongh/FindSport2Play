961b99ee74abb41f23eece6cb84ac649d360c057
Song <yousonghou@yahoo.com>
2018-08-13 16:05:52 +0000

    add image and improve home page

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,14 +1,32 @@
 import React, {Component} from 'react';
 
 import sportImage from '../../img/sport.png';
+import sportsImage from '../../img/sports.gif';
 
 class Home extends Component{
   render(){
       return(
           <div>
-              <h1 className="text-center">Welcome to FindSport2Play</h1>
-              <img className="mx-auto d-block" src={sportImage} alt="Sport" />
-          </div>
+            <header className="jumbotron bg-primary">
+          		<div className="container">
+          			<h1 className="text-center">Welcome to FindSport2Play</h1>
+          			<img className="img-fluid" id="imageSize" src={sportImage} alt="Sport" />
+          		</div>
+          	</header>
+        	
+            <div className="jumbotron bg-light">
+        		  <div className="container">
+        			  <div className="row">
+        				  <div className="col-md-5">
+        					  <img className="img-fluid rounded-circle float-right" src={sportsImage} alt="Sports"/>
+        				  </div>
+        				  <div className="col-md-7">
+        					  <h1 className="gj text-center">Start Finding Your Match!</h1>
+        				  </div>
+        			  </div>
+        		  </div>
+        	  </div>
+        	</div>
       );
   }
 }

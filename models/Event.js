6a568f6b2420f03fb11a90025c61c02cd856357a
Song <yousonghou@yahoo.com>
2018-08-15 04:18:59 +0000

    add event model

---
+++ models/Event.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const EventSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    nameofevent: {
+        type: String,
+        required: true
+    },
+    typeofsport: {
+        type: String,
+        required: true
+    },
+    numberofplayer: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Event = mongoose.model('event', EventSchema);

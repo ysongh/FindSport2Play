7f24f5c35b460e7348d758401fc4a8561e07bc25
Song <yousonghou@yahoo.com>
2018-08-20 17:40:25 +0000

    add connection to MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-const port = process.env.PORT || 8080;
+const port = 8081;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));

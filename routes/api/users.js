43965c3980b375997eac80a6cc7e7721d5b90b27
Song <yousonghou@yahoo.com>
2018-08-22 03:29:13 +0000

    install jsonwebtoken and add jwt to user login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
 
 const User = require('../../models/User');
 
@@ -45,7 +47,18 @@ router.post('/login', (req, res) => {
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if(isMatch){
-                        res.json({msg: 'Success'});
+                        const payload = {id: user.id, name: user.name};
+                        
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            {expiresIn: 3600},
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: 'Bearer ' + token
+                                });
+                        });
                     }
                     else{
                         return res.status(400).json({password: 'Password Incorrect'});

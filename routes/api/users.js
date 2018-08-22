453b52573008e26e42415450970119a2a5d55b99
Song <yousonghou@yahoo.com>
2018-08-22 02:20:54 +0000

    add user login route

--- routes/api/users.js
+++ routes/api/users.js
@@ -33,4 +33,25 @@ router.post('/register', (req, res) => {
         });
 });
 
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    
+    User.findOne({email})
+        .then(user => {
+            if(!user){
+                return res.status(404).json({email: 'User email not found'});
+            }
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch){
+                        res.json({msg: 'Success'});
+                    }
+                    else{
+                        return res.status(400).json({password: 'Password Incorrect'});
+                    }
+                });
+        });
+});
+
 module.exports = router;

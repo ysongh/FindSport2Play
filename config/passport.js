c876ec6fef4d98c9d6d36650d7d09435ae426372
Song <yousonghou@yahoo.com>
2018-08-23 04:54:41 +0000

    add JWT authentication

--- config/passport.js
+++ config/passport.js
@@ -10,6 +10,13 @@ opts.secretOrKey = keys.secretOrKey;
 
 module.exports = (passport) => {
     passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
-        console.log(jwt_payload);
+        User.findById(jwt_payload.id)
+            .then(user => {
+                if(user){
+                    return done(null, user);
+                }
+                return done(null, false);
+            })
+            .catch(err => console.log(err));
     }));
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -69,7 +69,11 @@ router.post('/login', (req, res) => {
 });
 
 router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
-    res.json({msg: 'Success'});
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    });
 });
 
 module.exports = router;

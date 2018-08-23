a68ea55aa14a18a7bb70661f85bbd6ddf869f7cd
Song <yousonghou@yahoo.com>
2018-08-23 18:19:30 +0000

    install validator and validation for register

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,15 +5,24 @@ const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
 const passport = require('passport');
 
+const validateRegisterInput = require('../../validation/register');
+
 const User = require('../../models/User');
 
 router.get('/test', (req, res) => res.json({msg: "Users Work"}));
 
 router.post('/register', (req, res) => {
+    const {errors, isValid} = validateRegisterInput(req.body);
+    
+    if(!isValid){
+        return res.status(400).json(errors);
+    }
+    
     User.findOne({ email: req.body.email })
         .then(user => {
             if(user){
-                return res.status(400).json({email: 'Email Already Exists'});
+                errors.email = 'Email Already Exists';
+                return res.status(400).json(errors);
             }
             else{
                 const newUser = new User({

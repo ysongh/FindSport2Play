81ea7662ec47fab9b5c86c236e02aa6f157a8dbf
Song <yousonghou@yahoo.com>
2018-08-23 20:54:18 +0000

    add validation for login

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,7 @@ const keys = require('../../config/keys');
 const passport = require('passport');
 
 const validateRegisterInput = require('../../validation/register');
+const validateLoginInput = require('../../validation/login');
 
 const User = require('../../models/User');
 
@@ -46,13 +47,19 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
+    const {errors, isValid} = validateLoginInput(req.body);
+    if(!isValid){
+        return res.status(400).json(errors);
+    }
+    
     const email = req.body.email;
     const password = req.body.password;
     
     User.findOne({email})
         .then(user => {
             if(!user){
-                return res.status(404).json({email: 'User email not found'});
+                errors.email = 'User not found';
+                return res.status(404).json(errors);
             }
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
@@ -71,7 +78,8 @@ router.post('/login', (req, res) => {
                         });
                     }
                     else{
-                        return res.status(400).json({password: 'Password Incorrect'});
+                        errors.password = 'Password Incorrect';
+                        return res.status(400).json(errors);
                     }
                 });
         });

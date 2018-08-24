970b9e0c7105fef23646cbbd56c6ff40fc35f28a
Song <yousonghou@yahoo.com>
2018-08-24 16:50:32 +0000

    install classname and add error handling and display on register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import classnames from 'classnames';
 
 import axios from '../../axios-lists';
 
@@ -32,59 +33,73 @@ class Register extends Component{
         
         axios.post('/api/users/register', newUser)
           .then(res => console.log(res.data))
-          .catch(err => console.log(err.response.data));
+          .catch(err => this.setState({errors: err.response.data}));
     }
     
     render(){
-        return(
-            <div className="register">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-md-8 m-auto">
-                      <h1 className="display-4 text-center">Sign Up</h1>
-                      <form onSubmit={this.onSubmit}>
-                        <div className="form-group">
-                          <input type="text"
-                            className="form-control form-control-lg"
-                            placeholder="Name"
-                            name="name"
-                            value={this.state.name}
-                            onChange={this.onChange} />
-                        </div>
-                        <div className="form-group">
-                          <input type="email"
-                            className="form-control form-control-lg"
-                            placeholder="Email Address"
-                            name="email"
-                            value={this.state.email}
-                            onChange={this.onChange} />
-                        </div>
-                        <div className="form-group">
-                          <input type="password"
-                            className="form-control form-control-lg"
-                            placeholder="Password"
-                            name="password"
-                            value={this.state.password}
-                            onChange={this.onChange} />
-                        </div>
-                        <div className="form-group">
-                          <input type="password"
-                            className="form-control form-control-lg"
-                            placeholder="Confirm Password"
-                            name="password2"
-                            value={this.state.password2}
-                            onChange={this.onChange} />
-                        </div>
-                        <input type="submit" className="btn btn-info btn-block mt-4" />
-                      </form>
-                      <Link to="/" className="btn btn-light">
-                          Go Back
-                      </Link>
-                    </div>
+      const {errors} = this.state;
+      
+      return(
+          <div className="register">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-8 m-auto">
+                    <h1 className="display-4 text-center">Sign Up</h1>
+                    <form noValidate onSubmit={this.onSubmit}>
+                      <div className="form-group">
+                        <input type="text"
+                          className={classnames('form-control form-control-lg', {
+                            'is-invalid': errors.name
+                          })}
+                          placeholder="Name"
+                          name="name"
+                          value={this.state.name}
+                          onChange={this.onChange} />
+                          {errors.name && (<div className="invalid-feedback">{errors.name}</div>)}
+                      </div>
+                      <div className="form-group">
+                        <input type="email"
+                          className={classnames('form-control form-control-lg', {
+                            'is-invalid': errors.email
+                          })}
+                          placeholder="Email Address"
+                          name="email"
+                          value={this.state.email}
+                          onChange={this.onChange} />
+                          {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
+                      </div>
+                      <div className="form-group">
+                        <input type="password"
+                          className={classnames('form-control form-control-lg', {
+                            'is-invalid': errors.password
+                          })}
+                          placeholder="Password"
+                          name="password"
+                          value={this.state.password}
+                          onChange={this.onChange} />
+                          {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
+                      </div>
+                      <div className="form-group">
+                        <input type="password"
+                          className={classnames('form-control form-control-lg', {
+                            'is-invalid': errors.password2
+                          })}
+                          placeholder="Confirm Password"
+                          name="password2"
+                          value={this.state.password2}
+                          onChange={this.onChange} />
+                          {errors.password2 && (<div className="invalid-feedback">{errors.password2}</div>)}
+                      </div>
+                      <input type="submit" className="btn btn-info btn-block mt-4" />
+                    </form>
+                    <Link to="/" className="btn btn-light">
+                        Go Back
+                    </Link>
                   </div>
                 </div>
-            </div>
-        );
+              </div>
+          </div>
+      );
     }
 }
 

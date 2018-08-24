5e383180b5363d100dff398305b54e5e2d70fb31
Song <yousonghou@yahoo.com>
2018-08-24 15:39:35 +0000

    install axios

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import axios from '../../axios-lists';
+
 class Register extends Component{
     constructor(){
         super();
@@ -28,7 +30,9 @@ class Register extends Component{
             password2: this.state.password2
         };
         
-        console.log(newUser);
+        axios.post('/api/users/register', newUser)
+          .then(res => console.log(res.data))
+          .catch(err => console.log(err.response.data));
     }
     
     render(){
--- server.js
+++ server.js
@@ -18,6 +18,18 @@ mongoose
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, HEAD');
+
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
 
 app.use(passport.initialize());
 require('./config/passport')(passport);

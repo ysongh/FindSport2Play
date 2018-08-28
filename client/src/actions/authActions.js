ff5910662b011e506322c65a46239cb58f6b8b47
Song <yousonghou@yahoo.com>
2018-08-28 02:50:52 +0000

    add utillis and install jwt-decode

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,13 +1,29 @@
 import axios from '../axios-lists';
+
+import setAuthToken from '../utilis/setAuthToken';
 import { GET_ERRORS } from './types';
 
 export const registerUser = (userData, history) => dispatch => {
-    axios.post('/api/users/register', userData)
-      .then(res => history.push('/login'))
-      .catch(err =>
-        dispatch({
-            type: GET_ERRORS,
-            payload: err.response.data
-        })
-      );
+  axios.post('/api/users/register', userData)
+    .then(res => history.push('/login'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+export const loginUser = (userData) => dispatch => {
+  axios.post('/api/users/login', userData)
+    .then(res => {
+      const { token } = res.data;
+      localStorage.setItem('jwtToken', token);
+      setAuthToken(token);
+    })
+    .catch(err => 
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+    }));
 };

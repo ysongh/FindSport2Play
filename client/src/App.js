050ff8973f5788bf84e65d71aa6521517d313285
Song <yousonghou@yahoo.com>
2018-08-30 15:58:27 +0000

    create login form functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utilis/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -8,6 +11,12 @@ import Home from './components/home/Home';
 import Events from './components/events/Events';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+
+if(localStorage.jwtToken){
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import classnames from 'classnames';
+
+import { loginUser } from '../../actions/authActions';
 
 class Login extends Component{
     constructor(){
@@ -13,21 +17,32 @@ class Login extends Component{
         this.onSubmit = this.onSubmit.bind(this);
     }
     
+    componentWillReceiveProps(nextProps){
+      if(nextProps.auth.isAuthenticated){
+        this.props.history.push('/events');
+      }
+      if(nextProps.errors){
+        this.setState({errors: nextProps.errors});
+      }
+    }
+    
     onChange(e){
         this.setState({[e.target.name]: e.target.value});
     }
     
     onSubmit(e){
         e.preventDefault();
-        const user = {
+        const userData = {
             email: this.state.email,
             password: this.state.password
         };
         
-        console.log(user);
+        this.props.loginUser(userData);
     }
     
     render(){
+        const {errors} = this.state;
+        
         return(
             <div className="login">
                 <div className="container">
@@ -38,20 +53,26 @@ class Login extends Component{
                         <div className="form-group">
                           <input
                             type="email"
-                            className="form-control form-control-lg"
+                            className={classnames('form-control form-control-lg', {
+                                'is-invalid': errors.email
+                            })}
                             placeholder="Email Address"
                             name="email"
                             value={this.state.email}
                             onChange={this.onChange} />
+                            {errors.email && (<div className="invalid-feedback">{errors.email}</div>)}
                         </div>
                         <div className="form-group">
                           <input
                             type="password"
-                            className="form-control form-control-lg"
+                             className={classnames('form-control form-control-lg', {
+                                'is-invalid': errors.password
+                            })}
                             placeholder="Password"
                             name="password"
                             value={this.state.password}
                             onChange={this.onChange} />
+                            {errors.password && (<div className="invalid-feedback">{errors.password}</div>)}
                         </div>
                         <input type="submit" className="btn btn-info btn-block mt-4" />
                       </form>
@@ -66,4 +87,9 @@ class Login extends Component{
     }
 }
 
-export default Login;
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps, {loginUser})(Login);

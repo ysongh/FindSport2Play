01c6b92a14c6ac5c4b89dcd24038f986be35bf7f
Song <yousonghou@yahoo.com>
2018-09-03 03:28:00 +0000

    add get allEvents route

--- routes/api/events.js
+++ routes/api/events.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Event = require('../../models/Event');
 
 router.get('/test', (req, res) => res.json({msg: "Events Work"}));
+
+router.get('/all', (req, res) => {
+    Event.find()
+        .populate('user', ['name'])
+        .then(events => {
+            res.json(events);
+        })
+        .catch(err => 
+            res.status(404).json({error: err})
+        );
+});
 
 module.exports = router;

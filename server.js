f17a1780c8a00a2692a5288e48631ac82bd8fbcc
Song <yousonghou@yahoo.com>
2018-09-11 04:23:02 +0000

    securing keys and prepare for deploy

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const passport = require('passport')
+const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const events = require('./routes/api/events');
@@ -37,6 +38,14 @@ require('./config/passport')(passport);
 app.use('/api/users', users);
 app.use('/api/events', events);
 
-const port = 8081;
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 8081;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));

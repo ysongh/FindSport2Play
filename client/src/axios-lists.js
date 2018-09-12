213e12e3a0b4629b29a541e62e976ad1e7a0c4f7
Song <yousonghou@yahoo.com>
2018-09-12 00:57:55 +0000

    change baseURL and add REDUX_DEVTOOLS_EXTENSION

--- client/src/axios-lists.js
+++ client/src/axios-lists.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-   baseURL: 'https://findsport2play.herokuapp.com/' 
+   baseURL: 'https://findsport2play-ysongh.c9users.io:8081' 
 });
 
 export default instance;
--- server.js
+++ server.js
@@ -46,6 +46,6 @@ if(process.env.NODE_ENV === 'production'){
     });
 }
 
-const port = process.env.PORT || 8081;
+const port = 8081;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));

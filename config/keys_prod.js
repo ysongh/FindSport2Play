b980601d61b71789c1d7c590251d4c7e771f51aa
Song <yousonghou@yahoo.com>
2018-09-12 21:55:15 +0000

    install cloudinary and add cloudinary keys

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,6 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
-    secretOrKey: process.env.SECRET_OR_KEY
+    secretOrKey: process.env.SECRET_OR_KEY,
+    cloudinaryAPIKey: process.env.CLOUDINARY_API_KEY,
+    cloudinaryAPISecret: process.env.CLOUDINARY_API_SECRET
 };

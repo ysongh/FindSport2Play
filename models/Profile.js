b7238b1c7b1aac40391ed721e879175b16eb1f1c
Song <yousonghou@yahoo.com>
2018-10-08 23:32:45 +0000

    add profile model

---
+++ models/Profile.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ProfileSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectID,
+        ref: 'users'
+    },
+    handle: {
+        type: String,
+        required: true,
+        max: 40
+    },
+    location: {
+        type: String
+    },
+    favoriteSport: {
+        type: [String],
+        required: true
+    },
+    bio: {
+        type: String
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);

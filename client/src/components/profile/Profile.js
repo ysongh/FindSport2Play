19e0ae4c0bca1e8787d498e99e7bea5ef061c878
Song <yousonghou@yahoo.com>
2018-10-08 05:11:08 +0000

    display user data on profile component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
+import { connect } from 'react-redux';
+
+import Spinner from '../common/Spinner'
+import { getUser } from '../../actions/userActions';
 
 class Profile extends Component{
+  componentDidMount(){
+     this.props.getUser(this.props.match.params.id);
+  }
     
   render(){
+      const {user, loading} = this.props.user;
+      
+      let userContent;
+        
+        if(user === null || loading){
+            userContent = <Spinner />;
+        }
+        else{
+            userContent = (
+                <div>
+                  <p>{user.name}</p>
+                  <p>{user.email}</p>
+                  <p><Moment format="MM/DD/YYYY">{user.date}</Moment></p>
+                </div>
+            );
+        }
+      
       return(
           <div>
             <h1 className="text-center">Profile</h1>
+            {userContent}
           </div>
       );
   }
 }
 
-export default Profile;
+const mapStateToProps = state => ({
+  user: state.users
+});
+
+export default connect(mapStateToProps, {getUser})(Profile); 

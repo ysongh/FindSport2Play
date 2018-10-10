5c2ac4ef3bb4a76371b2db52e628095fb1a1e387
Song <yousonghou@yahoo.com>
2018-10-10 03:21:58 +0000

    add get current user profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,19 @@ const passport = require('passport');
 
 const Profile = require('../../models/Profile');
 
+router.get('/', passport.authenticate('jwt', {session: false}),(req, res) => {
+    const errors = {};
+    Profile.findOne({user: req.user.id})
+        .then(profile => {
+            if(!profile){
+                errors.noprofile = 'There is no profile for this user';
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
+});
+
 router.post('/', passport.authenticate('jwt', {session: false}),(req, res) => {
     const profileFields = {};
     profileFields.user = req.user.id;

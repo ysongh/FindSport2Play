5f6d7adcc77a3d03792fe4264bd78e44f5668ba5
Song <yousonghou@yahoo.com>
2018-10-11 00:18:02 +0000

    add condition statement to show user profile or link to create profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -11,36 +11,51 @@ class Profile extends Component{
   }
     
   render(){
-      const {profile, loading} = this.props.profile;
-      
-      let profileContent;
-        
-        if(profile === null || loading){
-            profileContent = <Spinner />;
-        }
-        else{
-            profileContent = (
-                <div>
-                  <p>{profile.user.name}</p>
-                  <p>{profile.location}</p>
-                  <p>{profile.favoriteSport}</p>
-                  <p>{profile.bio}</p>
-                  <p><Moment format="MM/DD/YYYY">{profile.date}</Moment></p>
-                </div>
-            );
-        }
-      
-      return(
+    const {user} = this.props.auth;
+    const {profile, loading} = this.props.profile;
+    
+    let profileContent;
+    
+    if(profile === null || loading){
+        profileContent = <Spinner />;
+    }
+    else{
+      if(Object.keys(profile).length > 0){
+        profileContent = (
           <div>
-            <h1 className="text-center">Profile</h1>
-            {profileContent}
+            <p className="lead text-muted">{profile.user.name}</p>
+            <p>{profile.location}</p>
+            <p>{profile.favoriteSport}</p>
+            <p>{profile.bio}</p>
+            <p><Moment format="MM/DD/YYYY">{profile.date}</Moment></p>
           </div>
-      );
+        );
+      }
+      else{
+        profileContent = (
+          <div>
+            <p className="lead text-muted">{user.name}</p>
+            <p>You have not yet setup a profile, please add info</p>
+            <p className="btn btn-lg btn-info">
+              Create Profile
+            </p>
+          </div>
+        );
+      }
+    }
+    
+    return(
+        <div>
+          <h1 className="text-center">Profile</h1>
+          {profileContent}
+        </div>
+    );
   }
 }
 
 const mapStateToProps = state => ({
-  profile: state.profile
+  profile: state.profile,
+  auth: state.auth
 });
 
 export default connect(mapStateToProps, {getCurrentProfile})(Profile); 

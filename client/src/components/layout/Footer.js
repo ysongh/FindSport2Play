5d419788cbb4cd1f1cd4e1b7088a29bb42a1978f
Song <yousonghou@yahoo.com>
2019-02-09 05:31:22 +0000

    remove margin top

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default() => {
     return(
-        <footer className="bg-dark text-white mt-5 p-4 text-center">
+        <footer className="bg-dark text-white p-4 text-center">
             Copyright &copy;{new Date().getFullYear()} FindSport2Play
         </footer>
     );

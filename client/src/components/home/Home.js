2c5774bb5566e4ff7b0057e8c9bd6a196c915d1d
Song <yousonghou@yahoo.com>
2019-03-05 05:59:47 +0000

    make the background image take up the whole page

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -33,21 +33,20 @@ class Home extends Component{
           		<h1 className={styles.header__title}>Welcome to FindSport2Play</h1>
           		<p className={styles.header__text}>Find someone to play sport with</p>
           		{isAuthenticated ? userLinks : guestLinks}
+          		<div className={styles.event}>
+          		  <div className="container">
+          			  <div className="row">
+          				  <div className="col-md-5">
+          					  <img className="img-fluid rounded-circle float-right" src={sportsImage} alt="Sports"/>
+          				  </div>
+          				  <div className="col-md-7 text-center">
+          					  <h1 className={styles.event__title}>Start Finding Your Match!</h1>
+          					  <Link to="/events" className="btn btn-lg text-white bg-info">Go to Events</Link>
+          				  </div>
+          			  </div>
+          		  </div>
+          	  </div>
           	</header>
-
-            <div className={styles.event}>
-        		  <div className="container">
-        			  <div className="row">
-        				  <div className="col-md-5">
-        					  <img className="img-fluid rounded-circle float-right" src={sportsImage} alt="Sports"/>
-        				  </div>
-        				  <div className="col-md-7 text-center">
-        					  <h1 className={styles.event__title}>Start Finding Your Match!</h1>
-        					  <Link to="/events" className="btn btn-lg text-white bg-info">Go to Events</Link>
-        				  </div>
-        			  </div>
-        		  </div>
-        	  </div>
         	</div>
       );
   }

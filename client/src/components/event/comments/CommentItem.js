3f193d4cb035f3d91be6f01e29bfab84d66e33e0
Song <yousonghou@yahoo.com>
2019-03-06 20:16:10 +0000

    use flex to style the layout of the comment section

--- client/src/components/event/comments/CommentItem.js
+++ client/src/components/event/comments/CommentItem.js
@@ -15,29 +15,23 @@ class CommentItem extends Component{
         
         return(
             <div className="card card bg-light text-dark mb-1 p-2">
-              <div className="row">
-                <div className="col-md-1">
-                  <i className="far fa-user text-center"></i>
-                  <div>
-                     <Link to={`/profile/${comment.user}`}>
-                         <p className="font-italic">{comment.name}</p>
-                    </Link>
-                  </div>
-                  {event.user._id === auth.user.id ? (
-                    <button 
-                      onClick={this.onDeleteClick.bind(this, event._id, comment._id)}
-                      type="button"
-                      className="btn btn-danger mr-1" >
-                      <i className="fas fa-times" />
-                    </button>
-                   ) : null}
-                </div>
-                <div className="col-md-11">
-                  <p className="lead">
-                    {comment.text}
-                  </p>
-                </div>
-              </div>
+              <p className="lead">
+                {comment.text}
+              </p>
+              <div class="d-flex flex-row align-items-center">
+                <i className="far fa-user mr-2"></i>
+                 <Link to={`/profile/${comment.user}`}>
+                     <p className="font-italic d-inline">{comment.name}</p>
+                </Link>
+                {event.user._id === auth.user.id ? (
+                  <button 
+                    onClick={this.onDeleteClick.bind(this, event._id, comment._id)}
+                    type="button"
+                    className="btn btn-danger ml-auto p-2" >
+                    <i className="fas fa-times" />
+                  </button>
+                 ) : null}
+               </div>
             </div>
         );
     }

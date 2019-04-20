dd917b74c3186d06223cd728d4643a0f6b457285
Song <yousonghou@yahoo.com>
2019-04-20 01:17:14 +0000

    remove event information on the home page

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import styles from './Home.module.css';
-import sportsImage from '../../img/sports.gif';
 
 class Home extends Component{
   onLogoutClick(e){
@@ -33,19 +32,6 @@ class Home extends Component{
           		<h1 className={styles.header__title}>Welcome to FindSport2Play</h1>
           		<p className={styles.header__text}>Find someone to play sport with</p>
           		{isAuthenticated ? userLinks : guestLinks}
-          		<div className={styles.event}>
-          		  <div className="container">
-          			  <div className="row">
-          				  <div className="col-md-5">
-          					  <img className="img-fluid rounded-circle float-right" src={sportsImage} alt="Sports"/>
-          				  </div>
-          				  <div className="col-md-7 text-center">
-          					  <h1 className={styles.event__title}>Start Finding Your Match!</h1>
-          					  <Link to="/events" className="btn btn-lg text-white bg-info">Go to Events</Link>
-          				  </div>
-          			  </div>
-          		  </div>
-          	  </div>
           	</header>
         	</div>
       );

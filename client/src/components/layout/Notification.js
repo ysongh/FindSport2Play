76054fd5f4480555660c18a018b31f7b6b3a17ee
Song <yousonghou@yahoo.com>
2019-05-13 02:47:03 +0000

    display notification date

--- client/src/components/layout/Notification.js
+++ client/src/components/layout/Notification.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
+
+import styles from './Navbar.module.css';
 
 class Notification extends Component{
     render(){
         const {notification} = this.props;
         
         return(
-            <p>{notification.text} by {notification.authorName}</p>
+            <div>
+                <p className={styles.notification__date}><Moment format="MM/DD/YYYY">{notification.date}</Moment></p>
+                <p>{notification.text} by {notification.authorName}</p>
+                <hr />
+            </div>
+           
         );
     }
 }

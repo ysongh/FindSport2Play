3f1895151f8a6b309b6f30d8b925004cea079c16
You Song Hou <yousonghou@yahoo.com>
2020-05-17 20:53:06 -0400

    refactor create event page to use material-ui Grid

--- client/src/components/create-event/CreateEvent.js
+++ client/src/components/create-event/CreateEvent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { Grid } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 
 import TextFieldGroup from '../common/TextFieldGroup';
 import TextAreaFieldGroup from '../common/TextAreaFieldGroup';
@@ -58,88 +58,83 @@ class CreateEvent extends Component{
         const {errors} = this.state;
         
         return(
-            <div className="create-event mt-2 mb-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-8 m-auto">
-                            <h1 className="display-4 text-center">Host Your Event</h1>
-                            <small className="d-block pb-3">* = required fields</small>
-                            <form onSubmit={this.onSubmit}>
+            <Grid container justify="center">
+                <Grid item xs={12} sm={8} md={6}>
+                    <Typography variant="h3" component="h1" align="center" gutterBottom>
+                        Host Your Event
+                    </Typography>
+                    <form onSubmit={this.onSubmit}>
+                        <TextFieldGroup
+                            label="Event Name *"
+                            placeholder="Event Name"
+                            name="nameofevent"
+                            type="name"
+                            value={this.state.nameofevent}
+                            onChange={this.onChange}
+                            error={errors.nameofevent}
+                        />
+                        <Grid container spacing={3}>
+                            <Grid item xs={6}>
+                                <SelectFieldGroup
+                                    label="Type of Sport *"
+                                    name="typeofsport"
+                                    type="name"
+                                    value={this.state.typeofsport}
+                                    onChange={this.onChange}
+                                    error={errors.typeofsport}
+                                />
+                            </Grid>
+                            <Grid item xs={6}>
                                 <TextFieldGroup
-                                  label="Event Name *"
-                                  placeholder="Event Name"
-                                  name="nameofevent"
-                                  type="name"
-                                  value={this.state.nameofevent}
-                                  onChange={this.onChange}
-                                  error={errors.nameofevent}
+                                    label="Number of player *"
+                                    placeholder="Number of player"
+                                    name="numberofplayer"
+                                    type="number"
+                                    value={this.state.numberofplayer}
+                                    onChange={this.onChange}
+                                    error={errors.numberofplayer}
                                 />
-                                <Grid container spacing={3}>
-                                    <Grid item xs={6}>
-                                        <SelectFieldGroup
-                                            label="Type of Sport *"
-                                            name="typeofsport"
-                                            type="name"
-                                            value={this.state.typeofsport}
-                                            onChange={this.onChange}
-                                            error={errors.typeofsport}
-                                        />
-                                    </Grid>
-                                    <Grid item xs={6}>
-                                        <TextFieldGroup
-                                            label="Number of player *"
-                                            placeholder="Number of player"
-                                            name="numberofplayer"
-                                            type="number"
-                                            value={this.state.numberofplayer}
-                                            onChange={this.onChange}
-                                            error={errors.numberofplayer}
-                                        />
-                                    </Grid>
-                                </Grid>
-                                
-                                
-                                <TextFieldGroup
-                                  label="Image URL"
-                                  placeholder="Image URL"
-                                  name="imageURL"
-                                  type="name"
-                                  value={this.state.imageURL}
-                                  onChange={this.onChange}
-                                  error={errors.imageURL}
-                                />
-                                <TextFieldGroup
-                                  label="Location"
-                                  placeholder="Location"
-                                  name="location"
-                                  type="name"
-                                  value={this.state.location}
-                                  onChange={this.onChange}
-                                  error={errors.location}
-                                />
-                                <TextFieldGroup
-                                  label="Date"
-                                  name="start"
-                                  type="date"
-                                  value={this.state.start}
-                                  onChange={this.onChange}
-                                  error={errors.start}
-                                />
-                                <TextAreaFieldGroup
-                                  label="Description"
-                                  placeholder="Details about this event"
-                                  name="description"
-                                  type="name"
-                                  value={this.state.description}
-                                  onChange={this.onChange}
-                                  error={errors.description}
-                                />
-                                <input type="submit" className="btn btn-info btn-block mt-4" />
-                          </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                            </Grid>
+                        </Grid>
+                        <TextFieldGroup
+                            label="Image URL"
+                            placeholder="Image URL"
+                            name="imageURL"
+                            type="name"
+                            value={this.state.imageURL}
+                            onChange={this.onChange}
+                            error={errors.imageURL}
+                        />
+                        <TextFieldGroup
+                            label="Location"
+                            placeholder="Location"
+                            name="location"
+                            type="name"
+                            value={this.state.location}
+                            onChange={this.onChange}
+                            error={errors.location}
+                        />
+                        <TextFieldGroup
+                            label="Date"
+                            name="start"
+                            type="date"
+                            value={this.state.start}
+                            onChange={this.onChange}
+                            error={errors.start}
+                        />
+                        <TextAreaFieldGroup
+                            label="Description"
+                            placeholder="Details about this event"
+                            name="description"
+                            type="name"
+                            value={this.state.description}
+                            onChange={this.onChange}
+                            error={errors.description}
+                        />
+                        <input type="submit" className="btn btn-info btn-block mt-4" />
+                    </form>
+                </Grid>
+            </Grid>
         );
     }
 }

bc6c288974a2c8d717da938685a151bfc6fee8e8
You Song Hou <yousonghou@yahoo.com>
2020-06-05 20:42:29 -0400

    move mobile navbar component to its own file

--- client/src/components/layout/navbar/Navbar.js
+++ client/src/components/layout/navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { Container, AppBar, Toolbar, Drawer, List, ListItem, ListItemText, Badge, Button, Link, IconButton } from '@material-ui/core';
+import { Container, AppBar, Toolbar, Drawer, List, ListItem, ListItemText, Badge, Link, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 
 import { logoutUser } from '../../../actions/authActions';
 import { checkNotification } from '../../../actions/notificationActions';
 import Desktop from './Desktop';
+import Mobile from './Mobile';
 import NotificationList from '../../notification/NotificationList';
 import UserMenu from '../UserMenu';
 
@@ -80,34 +81,6 @@ class Navbar extends Component {
       </Badge>
     );
 
-    // Start of Desktop Navbar
-    
-    // End of Desktop Navbar
-
-    // Start of Mobile Navbar
-    const sideAuthLinks = (
-      <div>
-        <ListItem button component={RouterLink} to="/profile">
-          <ListItemText primary="Your Profile" />
-        </ListItem>
-        <ListItem button onClick={this.onLogoutClick.bind(this)}>
-          <ListItemText primary="Logout" />
-        </ListItem>
-      </div>
-    );
-
-    const sideGuestLinks = (
-      <div>
-        <ListItem button component={RouterLink} to="/login" color="textPrimary">
-          <ListItemText primary="Login" />
-        </ListItem>
-        <ListItem button component={RouterLink} to="/register" color="textPrimary">
-          <ListItemText primary="Get Started" />
-        </ListItem>
-      </div>
-    );
-    // End of Mobile Navbar
-
     const sideDrawer = (
       <List component="nav">
         <ListItem button component={RouterLink} to="/">
@@ -119,7 +92,9 @@ class Navbar extends Component {
         <ListItem button component={RouterLink} to="/create-event">
           <ListItemText primary="Create Event" />
         </ListItem>
-        {isAuthenticated ? sideAuthLinks : sideGuestLinks}
+        <Mobile
+          isAuthenticated={isAuthenticated}
+          logout={this.onLogoutClick.bind(this)} />
       </List>
     );
     

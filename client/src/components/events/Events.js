80608e0bd386c555d3a27ef839922fe6eb992622
You Song Hou <yousonghou@yahoo.com>
2020-08-10 23:48:42 -0400

    add a select field on the list of events page

--- client/src/components/events/Events.js
+++ client/src/components/events/Events.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Grid } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 
 import EventsList from './EventsList';
+import SelectFieldGroup from '../common/SelectFieldGroup';
 import Spinner from '../common/Spinner';
 import { getEvents } from '../../actions/eventActions';
 
+const sportList = ["Badminton", "Tennis", "Volleyball", "Basketball", "Baseball", "Running", "Table tennis", "Football", "Soccer"];
+
 class Events extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            typeofsport: ''
+        };
+        this.onChange = this.onChange.bind(this);
+    }
     componentDidMount(){
         window.scrollTo(0, 0);
         this.props.getEvents();
+    }
+
+    onChange(e){
+        this.setState({[e.target.name]: e.target.value});
     }
     
     render(){
@@ -29,8 +43,23 @@ class Events extends Component{
         
         return(
             <div>
-                <h1 className="text-center mt-2">List of Events</h1>
-
+                <Grid container className="marginX-1">
+                    <Grid item xs={12} sm={6} md={6}>
+                        <Typography className="marginT-2" variant="h3" component="h1">
+                            List of Events
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6}>
+                    <SelectFieldGroup
+                        label="Search Sport by Type"
+                        name="typeofsport"
+                        type="name"
+                        value={this.state.typeofsport}
+                        onChange={this.onChange}
+                        sportList={sportList}
+                    />
+                    </Grid>
+                </Grid>
                 {eventContent}
             </div>
         );

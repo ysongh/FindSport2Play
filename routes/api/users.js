173218ad654b72d6dea02ab9f6fabb8025c66a72
You Song Hou <yousonghou@yahoo.com>
2020-10-07 23:31:42 -0400

    update register route to send token to the client

--- routes/api/users.js
+++ routes/api/users.js
@@ -45,7 +45,20 @@ router.post('/register', (req, res) => {
                         newUser.password = hash;
                         newUser
                             .save()
-                            .then(user => res.json(user))
+                            .then(user => {
+                                const payload = {id: user.id, name: user.name};
+                                
+                                jwt.sign(
+                                    payload,
+                                    keys.secretOrKey,
+                                    {expiresIn: 3600},
+                                    (err, token) => {
+                                        res.json({
+                                            success: true,
+                                            token: 'Bearer ' + token
+                                        });
+                                });
+                            })
                             .catch(err => console.log(err));
                     });
                 });

c764d1a3af854cdb38cb67e14303eff4df70ad76
You Song Hou <yousonghou@yahoo.com>
2020-10-10 00:50:50 -0400

    add quick links section on the footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import { AppBar, Typography } from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
+import { AppBar, Container, Grid, Link, Typography } from '@material-ui/core';
+
+import Logo from '../../img/logo.png';
 
 export default() => {
     return(
         <footer>
             <AppBar className="primary-color marginT-3 pad-2" position="static">
-                <Typography align="center">
-                    Copyright &copy;{new Date().getFullYear()} FindSport2Play
-                </Typography>
+                <Grid component={Container} container>
+                    <Grid item xs={12} sm={3}>
+                        <img src={Logo} className="logo" alt="Logo" />
+                    </Grid>
+                    <Grid item xs={12} sm={9} className="footer-links">
+                        <Link className="white-link" component={RouterLink} to="/">
+                            Home
+                        </Link>
+                        <span>/</span>
+                        <Link className="white-link" component={RouterLink} to="/events">
+                            Events List
+                        </Link>
+                        <span>/</span>
+                        <Link className="white-link" component={RouterLink} to="/events">
+                            Create Event
+                        </Link>
+                        <span>/</span>
+                        <Link className="white-link" component={RouterLink} to="/events">
+                            Get Started
+                        </Link>
+                    </Grid>
+                    <Grid item xs={12} sm={3}></Grid>
+                    <Grid item xs={12} sm={9}>
+                        <Typography className="marginL-d7">
+                            Copyright &copy;{new Date().getFullYear()} FindSport2Play
+                        </Typography>
+                    </Grid>
+                </Grid>
             </AppBar>
         </footer>
     );
